0123e8b1e5099b8a28a0a265ca95dcf4cb9bc710
Muhib Mehdi <muhibmehdi24@gmail.com>
2025-09-03 23:29:13 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio
+My personal Portfolio, Projects and apps
+You will find my achievements here and also all my personal projects which you can also find on my github profile as they are hosted from there
---
+++ assets/vendor/php-email-form/validate.js
@@ -0,0 +1,52 @@
+(function () {
+  "use strict";
+
+  let forms = document.querySelectorAll('.php-email-form');
+
+  forms.forEach(function (e) {
+    e.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      let thisForm = this;
+      let action = thisForm.getAttribute('action');
+
+      if (!action) {
+        displayError(thisForm, 'The form action property is not set!');
+        return;
+      }
+
+      thisForm.querySelector('.loading').classList.add('d-block');
+      thisForm.querySelector('.error-message').classList.remove('d-block');
+      thisForm.querySelector('.sent-message').classList.remove('d-block');
+
+      let formData = new FormData(thisForm);
+
+      fetch(action, {
+        method: 'POST',
+        body: formData,
+        headers: { 'Accept': 'application/json' }
+      })
+      .then(response => {
+        thisForm.querySelector('.loading').classList.remove('d-block');
+        if (response.ok) {
+          thisForm.querySelector('.sent-message').classList.add('d-block');
+          thisForm.reset();
+        } else {
+          response.json().then(data => {
+            displayError(thisForm, data.error || 'Form submission failed.');
+          });
+        }
+      })
+      .catch((error) => {
+        displayError(thisForm, error.message);
+      });
+    });
+  });
+
+  function displayError(thisForm, error) {
+    thisForm.querySelector('.loading').classList.remove('d-block');
+    thisForm.querySelector('.error-message').innerHTML = error;
+    thisForm.querySelector('.error-message').classList.add('d-block');
+  }
+
+})();
